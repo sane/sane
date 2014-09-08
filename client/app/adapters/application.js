557f9c49efa68d632b73076d88fdd97c677199ff
Globegitter <markus.padourek@gmail.com>
2014-09-08 15:33:09 +0100

    Activate coalesceFindRequests for Ember Data beta-9.

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
+  coalesceFindRequests: true,
   namespace: 'api/v1',
   //this is dependent on production/development environment 
   //It is configured in config/environment.js
