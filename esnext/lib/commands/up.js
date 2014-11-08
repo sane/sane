84eab81a721291e2d405765d9266a0ae4d75e886
Globegitter <markus.padourek@gmail.com>
2014-11-08 14:39:31 +0000

    Added esnext folder that is used by esnext-cli to compile files to original path.

---
+++ esnext/lib/commands/up.js
@@ -0,0 +1,15 @@
+function timeout(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+async function f() {
+  console.log(1);
+  await timeout(1000);
+  console.log(2);
+  await timeout(1000);
+  console.log(3);
+}
+
+module.exports = function up() {
+  f();
+}
