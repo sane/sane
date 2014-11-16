efc499f391a523395273c9189d12a8802a255cb1
Globegitter <markus.padourek@gmail.com>
2014-11-16 13:36:51 +0000

    Compiled files with new verbose flag

--- esnext/lib/commands/up.js
+++ esnext/lib/commands/up.js
@@ -1,15 +1,24 @@
-function timeout(ms) {
-  return new Promise((res) => setTimeout(res, ms));
-}
+var spawn = require('child_process').spawn;
 
-async function f() {
-  console.log(1);
-  await timeout(1000);
-  console.log(6);
-  await timeout(1000);
-  console.log(3);
-}
+// function timeout(ms) {
+//   return new Promise((res) => setTimeout(res, ms));
+// }
+
+// async function f() {
+//   console.log(1);
+//   await timeout(1000);
+//   console.log(6);
+//   await timeout(1000);
+//   console.log(3);
+// }
 
 module.exports = function up() {
-  f();
+  // f();
+
+  var child = spawn('ember serve --proxy 192.168.59.103', [
+    'arg1', 'arg2', 'arg3' ]);
+// use event hooks to provide a callback to execute when data are available:
+child.stdout.on('data', function(data) {
+  console.log(data.toString());
+});
 }
