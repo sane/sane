f32ca74d28cae9b05ed0f24f66454657b9f8e03b
Globegitter <markus.padourek@gmail.com>
2014-12-10 01:34:22 +0000

    Added 6to5 library for now to improve ES6/7 performance a bit.

---
+++ src/tasks/trackCommand.js
@@ -0,0 +1,22 @@
+/*
+ *
+ */
+
+ module.exports = function trackCommand(name, options, leek) {
+  var optionString = '';
+  for (var opt in options) {
+    //exclude properties that are not cli options
+    if (options.hasOwnProperty(opt)
+      && !opt.startsWith('_')
+      && ['commands', 'options', 'parent'].indexOf(opt) === -1) {
+      optionString += `--${opt}=${options[opt]} `;
+    }
+  }
+
+  name = `${name} ${optionString}`;
+
+  leek.track({
+    name: 'sane ',
+    message: name
+  });
+}
