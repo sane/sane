53b90fb5b823c82a9430384caeed9f46bd666f7a
Kris Williams <kris@kris.net>
2014-12-23 13:07:30 -0800

    Replace intro video thumbnail with YouTube embed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+(function($) {
+  var $document = $(document);
+  
+  // replace intro video thumbnail with youtube embed
+  $document.on('flatdoc:ready', function() {
+    $('#sane-stack-quickstart').prev('p').find('> a')
+      .replaceWith('<iframe width="854" height="510" src="//www.youtube.com/embed/zWU6dRNpNtc" frameborder="0" allowfullscreen></iframe>')
+  })
+  
+})(jQuery);
