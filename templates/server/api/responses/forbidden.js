5b2081e1fd341d8a20a9c76e462eee2198e76c6d
Globegitter <markus.padourek@gmail.com>
2015-02-10 17:39:18 +0000

    Overwrite default response files to always return json since views are not needed.

---
+++ templates/server/api/responses/forbidden.js
@@ -0,0 +1,40 @@
+/**
+ * 403 (Forbidden) Handler
+ *
+ * Usage:
+ * return res.forbidden();
+ * return res.forbidden(err);
+ * return res.forbidden(err, 'some/specific/forbidden/view');
+ *
+ * e.g.:
+ * ```
+ * return res.forbidden('Access denied.');
+ * ```
+ */
+
+module.exports = function forbidden (data, options) {
+
+  // Get access to `req`, `res`, & `sails`
+  var req = this.req;
+  var res = this.res;
+  var sails = req._sails;
+
+  // Set status code
+  res.status(403);
+
+  // Log error to console
+  if (data !== undefined) {
+    sails.log.verbose('Sending 403 ("Forbidden") response: \n',data);
+  }
+  else sails.log.verbose('Sending 403 ("Forbidden") response');
+
+  // Only include errors in response if application environment
+  // is not set to 'production'.  In production, we shouldn't
+  // send back any identifying information about errors.
+  if (sails.config.environment === 'production') {
+    data = undefined;
+  }
+
+  // If the user-agent wants JSON, always respond with JSON
+  return res.jsonx(data);
+};
---
+++ templates/server/api/responses/serverError.js
@@ -0,0 +1,40 @@
+/**
+ * 500 (Server Error) Response
+ *
+ * Usage:
+ * return res.serverError();
+ * return res.serverError(err);
+ * return res.serverError(err, 'some/specific/error/view');
+ *
+ * NOTE:
+ * If something throws in a policy or controller, or an internal
+ * error is encountered, Sails will call `res.serverError()`
+ * automatically.
+ */
+
+module.exports = function serverError (data, options) {
+
+  // Get access to `req`, `res`, & `sails`
+  var req = this.req;
+  var res = this.res;
+  var sails = req._sails;
+
+  // Set status code
+  res.status(500);
+
+  // Log error to console
+  if (data !== undefined) {
+    sails.log.error('Sending 500 ("Server Error") response: \n',data);
+  }
+  else sails.log.error('Sending empty 500 ("Server Error") response');
+
+  // Only include errors in response if application environment
+  // is not set to 'production'.  In production, we shouldn't
+  // send back any identifying information about errors.
+  if (sails.config.environment === 'production') {
+    data = undefined;
+  }
+
+  // If the user-agent wants JSON, always respond with JSON
+  return res.jsonx(data);
+};
