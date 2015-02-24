d5c1beac3925663a0ead237d6b1481ab218943f2
Globegitter <markus.padourek@gmail.com>
2015-02-24 16:01:53 +0000

    Got sane-auth addon working in getAddonTest.js file. Still very WIP

--- tests/unit/getAddonTest.js
+++ tests/unit/getAddonTest.js
@@ -4,7 +4,13 @@ var { expect } = require('chai');
 var Project = require('../../lib/tasks/Project');
 var pluck = require('lodash/collection/pluck');
 var path = require('path');
-
+// var { ember } = require('../helpers/acceptanceSetup');
+// var { spawn } = require('child-process-promise');
+var { spawn }   = require('child_process');
+var installNpmPackage = require('../../lib/tasks/installNpmPackage');
+var getTemplates = require('../../lib/tasks/getTemplates');
+var copyToProject = require('../../lib/tasks/copyToProject');
+var fs = require('fs-extra');
 
 describe('getAddon tests', async function() {
 
@@ -17,25 +23,54 @@ describe('getAddon tests', async function() {
     console.log(project.getBlueprintPath(supportedBlueprints[0]));
     var blueprintPath = project.getBlueprintPath(supportedBlueprints[0]);
     console.log(blueprintPath);
+    // console.log(project.root);
+    // process.exit(0);
     // var generatePath = path.join(blueprintPath, 'generate');
     // var client = require(path.join(generatePath, 'client'));
     // var server = require(path.join(generatePath, 'client'));
     // console.log(client.fn());
-    var { client, server } = require(blueprintPath);
-    client.exec({
-      success: function(actions){
-        console.log(actions);
-      }
-    });
 
-    server.exec({
-      success: function(actions){
-        console.log(actions);
-      }
-    });
+    // var blueprint = require(blueprintPath);
+    // blueprint.client.exec({
+    //   success: function(actions) {
+    //     console.log('Client:');
+    //     console.log(actions.addNpmPackages);
+    //     console.log(actions.addEmberAddons);
+    //     for (var emberAddon of actions.addEmberAddons) {
+    //       // console.log(emberAddon);
+    //       var addonSemVer = emberAddon.name + '@' + emberAddon.target
+    //       var emberArgs = ['install:addon', addonSemVer]
+    //       // console.log(emberArgs);
+    //       spawn('ember', emberArgs, { stdio: 'inherit', cwd: path.join(project.root, 'client') });
+    //     }
+    //
+    //   }
+    // });
+    //
+    // blueprint.server.exec({
+    //   success: function(actions) {
+    //     console.log('Server!');
+    //     console.log(actions.addNpmPackages);
+    //     var serverPrefix = path.join(project.root, 'server');
+    //     for (var npmPackage of actions.addNpmPackages) {
+    //       // var emberArgs = ['install', npmPackage, '']
+    //       // console.log(npmPackage.name + '@' + npmPackage.target)
+    //       var packageSemVer = npmPackage.name + '@' + npmPackage.target
+    //       installNpmPackage([packageSemVer], { save: true, prefix: serverPrefix }).then(function(result){
+    //         console.log(result + 'has been installed');
+    //       });
+    //     }
+    //   }
+    // });
 
     //now copy over templates
-    // console.log();
+    var templates = getTemplates(path.join(blueprintPath, 'generate'));
+    // console.log(templates);
+    console.log('Copy over templates...');
+    copyToProject(templates, project.root);
+    console.log('done');
+    // fs.outputFileSync(templateOutPath, fs.readFileSync(templateInPath));
+    // // console.log();
     // console.log(addons.pkg);
     expect('1').to.equal('1');
   });
