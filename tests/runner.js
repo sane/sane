ae7dbce3578639735eaf33522ec5e2d69be85597
Globegitter <markus.padourek@gmail.com>
2015-04-13 22:29:58 +0100

    Using forked enpeem module to install packages. Test passing.

--- tests/runner.js
+++ tests/runner.js
@@ -21,7 +21,7 @@ function addFiles(mocha, files) {
   glob.sync(root + files).forEach(mocha.addFile.bind(mocha));
 }
 
-addFiles(mocha, '/**/installTest.js');
+addFiles(mocha, '/**/*Test.js');
 // addFiles(mocha, '/**/getAddonTest.js');
 
 // if (arg === 'all') {
