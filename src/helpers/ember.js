9a23e52f8d266aecbd529e75b48d3b88d074577d
Ian VanSchooten <ian@badgelabsllc.com>
2015-05-12 21:36:56 -0400

    Use SANE's ember if no global ember exists
    Previously, the `ember` helper would use the ember-cli closest to the
    process.cwd, and if it couldn't find one in the current project, it would
    use a global ember-cli.
    
    The problem is that upon first installation of `sane-cli`, unless `ember-cli`
    was also installed, the helper could not resolve a version of ember unless `sane` was
    invoked inside the sane project.  This meant that commands like `sane new .` and even
    `sane --help` would fail with `Error: not found: ember`.
    
    The changes here will cause the ember helper to use the ember-cli within node_modules of
    the globally installed sane-cli, much like the code in `commands/new.js`.
    
    With this commit in place, installing `ember-cli` globally should no longer be necessary.

--- src/helpers/ember.js
+++ src/helpers/ember.js
@@ -1,6 +1,16 @@
 'use strict';
 
-var which = require('npm-which')(process.cwd());
+var ember;
+var path             = require('path');
+var emberBin         = (process.platform === 'win32' ? 'ember.cmd' : 'ember');
+var localEmber       = path.join(__dirname, '..', '..', 'node_modules', '.bin', emberBin);
+var checkEnvironment = require('../tasks/checkEnvironment');
+var which            = require('npm-which')(process.cwd());
 
+if (!checkEnvironment.emberExists()) {
+  ember = localEmber;
+} else {
+  ember = which.sync('ember');
+}
 
-module.exports = which.sync('ember');
+module.exports = ember;
